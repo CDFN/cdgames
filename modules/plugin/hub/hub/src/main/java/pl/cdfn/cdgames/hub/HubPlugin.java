a7f6baee091604c924b5aea16f81bfe6fac64def
CDFN <codefun@protonmail.com>
2021-03-16 19:24:41 +0100

    adventure UTF8ResourceBundleControl

--- modules/plugin/hub/hub/src/main/java/pl/cdfn/cdgames/hub/HubPlugin.java
+++ modules/plugin/hub/hub/src/main/java/pl/cdfn/cdgames/hub/HubPlugin.java
@@ -7,6 +7,7 @@ import java.util.PropertyResourceBundle;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.translation.GlobalTranslator;
 import net.kyori.adventure.translation.TranslationRegistry;
+import net.kyori.adventure.util.UTF8ResourceBundleControl;
 import org.apache.logging.log4j.Logger;
 import org.spongepowered.api.event.EventManager;
 import org.spongepowered.api.event.Listener;
@@ -15,10 +16,9 @@ import org.spongepowered.api.util.locale.Locales;
 import org.spongepowered.plugin.PluginContainer;
 import org.spongepowered.plugin.jvm.Plugin;
 import pl.cdfn.cdgames.hub.event.caller.PlayerJoinSettingsCaller;
-import pl.cdfn.cdgames.hub.item.listener.PlayerItemListener;
 import pl.cdfn.cdgames.hub.item.listener.PlayerConnectionListener;
+import pl.cdfn.cdgames.hub.item.listener.PlayerItemListener;
 import pl.cdfn.cdgames.hub.player.repository.module.HubPlayerRepositoryModule;
-import resourcebundle.UTF8Control;
 
 @Plugin("cdgames_hub")
 public class HubPlugin {
@@ -46,9 +46,8 @@ public class HubPlugin {
 
     var registry = TranslationRegistry.create(Key.key("hub", "hub_translation_registry"));
     GlobalTranslator.get().addSource(registry);
-
     for (var locale : SUPPORTED_LOCALES) {
-      var bundle = PropertyResourceBundle.getBundle("hub", locale, new UTF8Control());
+      var bundle = PropertyResourceBundle.getBundle("hub", locale, UTF8ResourceBundleControl.get());
       logger.info("Registering locale {}", locale);
       registry.registerAll(locale, bundle, true);
     }
